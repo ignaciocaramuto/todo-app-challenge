9911b9d9d37c64442051a794f6343a0bf31b5372
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-05-25 15:44:40 -0300

    wip: first subscribe to getAllTask observable

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoListService } from 'src/app/services/todo-list.service';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+
+  constructor(private todoListService: TodoListService) {}
+
+  ngOnInit(): void {
+    this.todoListService.getAllTasks().subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (error) => console.error(error)
+    });
+  }
 
 }
