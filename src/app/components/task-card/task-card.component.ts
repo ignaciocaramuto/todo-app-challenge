cb26a8bf95ff5dd7cb6d8369e316e67d6da80ac0
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-05-26 09:45:48 -0300

    new: component tast-card to list tasks

---
+++ src/app/components/task-card/task-card.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Priority, Task } from 'src/app/interfaces/task.entities';
+
+@Component({
+  selector: 'app-task-card',
+  templateUrl: './task-card.component.html',
+  styleUrls: ['./task-card.component.scss']
+})
+export class TaskCardComponent {
+
+  priorities = Object.values(Priority);
+
+  @Input() task!: Task;
+
+  @Output() deleteTaskEmitter = new EventEmitter<Task>;
+  @Output() updateTaskPriorityEmitter = new EventEmitter<any>;
+  @Output() updateTaskDoneEmitter = new EventEmitter<any>;
+
+  public get priority(): typeof Priority {
+    return Priority; 
+  }
+
+  deleteTask(): void {
+    this.deleteTaskEmitter.emit(this.task);
+  }
+
+  updateTaskPriority(event: any): void {
+    this.updateTaskPriorityEmitter.emit({task: this.task, priority: event.target.value});
+  }
+
+  updateTaskDone(event: any): void {
+    console.log(event.checked);
+    
+    this.updateTaskDoneEmitter.emit({task: this.task, checked: event.checked})
+  }
+
+}
