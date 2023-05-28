d023c821f99cb234bcc5044950b390b5c947a075
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-05-27 22:41:44 -0300

    bugFix: put back test files into ./

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/todo-app-challenge'),
+      dir: require('path').join(__dirname, './../../coverage/todo-app-challenge'),
       subdir: '.',
       reporters: [
         { type: 'html' },
